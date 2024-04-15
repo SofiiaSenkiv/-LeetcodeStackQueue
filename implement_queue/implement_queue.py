714be52e47f0c2994574f46f7b9834333f71f690
SofiiaSenkiv <senkivsofijka@gmail.com>
2024-04-15 16:16:23 +0300

    added class Stack

---
+++ implement_queue/implement_queue.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def peek(self):
+        return self.items[-1]
+    def is_empty(self):
+        return len(self.items) == 0
+    
+class MyQueue:
+
+    def __init__(self):
+        pass
+
+    def push(self, x: int) -> None:
+        pass
+
+    def pop(self) -> int:
+        pass
+
+    def peek(self) -> int:
+        pass
+
+    def empty(self) -> bool:
+        pass
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
