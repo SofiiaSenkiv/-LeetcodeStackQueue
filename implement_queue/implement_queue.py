f9d4deb8a8c913abdd42d9f8060a35f5d0c5db4c
SofiiaSenkiv <senkivsofijka@gmail.com>
2024-04-15 16:30:27 +0300

    improved code, without errors

--- implement_queue/implement_queue.py
+++ implement_queue/implement_queue.py
@@ -11,26 +11,23 @@ class Stack:
         return len(self.items) == 0
     
 class MyQueue:
-
     def __init__(self):
         self.stack1=Stack()
         self.stack2=Stack()
-
-    def push(self, x: int) -> None:
+        self.front = None
+    def push(self, x):
+        if self.stack1.is_empty():
+            self.front = x
         self.stack1.push(x)
-
-    def pop(self) -> int:
+    def pop(self):
         if self.stack2.is_empty():
             while not self.stack1.is_empty():
                 self.stack2.push(self.stack1.pop())
-            return self.stack2.pop()
-
-    def peek(self) -> int:
-        if self.stack2.is_empty():
-            while not self.stack1.is_empty():
-                self.stack2.push(self.stack1.pop())
-        return self.stack2.peek()
-
+        return self.stack2.pop()
+    def peek(self):
+        if not self.stack2.is_empty():
+            return self.stack2.peek()
+        return self.front
     def empty(self) -> bool:
         return self.stack1.is_empty() and self.stack2.is_empty()
 
