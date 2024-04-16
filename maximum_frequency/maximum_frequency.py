8cd72957aff1784c01ac56aec8244e9572cd68f2
SofiiaSenkiv <senkivsofijka@gmail.com>
2024-04-16 14:59:47 +0300

    changed(defaultdict)

--- maximum_frequency/maximum_frequency.py
+++ maximum_frequency/maximum_frequency.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Node:
     def __init__(self, item, next=None):
         self.item = item
@@ -16,38 +17,14 @@ class Stack:
     @property
     def peek(self):
         return self.head.item
-class Queue:
-    def __init__(self):
-        self.head = None
-        self.size = 0
-    def is_empty(self):
-        return self.head is None
-    def pop(self):
-        if self.head:
-            item = self.head
-            self.head = self.head.next
-            self.size -= 1
-            return item
-        raise ValueError('Queue is empty.')
-    def push(self, item):
-        self.head = Node(item, self.head)
-        self.size += 1
-    @property
-    def peek(self):
-        return self.head.item
-
 class FreqStack:
     def __init__(self):
-        self.freq = {}
-        self.group = {}
+        self.freq = defaultdict(int)
+        self.group = defaultdict(Stack)
         self.max_freq = 0
     def push(self, val):
-        if val not in self.freq:
-            self.freq[val] = 0
         self.freq[val] += 1
         frequency = self.freq[val]
-        if frequency not in self.group:
-            self.group[frequency] = Stack()
         self.group[frequency].push(val)
         self.max_freq = max(self.max_freq, frequency)
     def pop(self):
@@ -58,9 +35,9 @@ class FreqStack:
             self.max_freq -= 1
         return val
 
-
-
-# Your FreqStack object will be instantiated and called as such:
-# obj = FreqStack()
-# obj.push(val)
-# param_2 = obj.pop()
+# Приклад використання:
+# freqStack = FreqStack()
+# freqStack.push(1)
+# freqStack.push(2)
+# freqStack.pop()
+    
